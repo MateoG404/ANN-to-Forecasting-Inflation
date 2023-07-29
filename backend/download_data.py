bee83174d071dd50a707f63f72f056af3c103ad0
Mateo Gutierrez <mgutierrezca@unal.edu.co>
2023-07-29 11:57:19 -0500

    Update download_data using OOP and create the script main to run the script download_data

--- backend/download_data.py
+++ backend/download_data.py
@@ -1,103 +1,122 @@
 import os
 import pandas as pd
 import requests
+import sys
 import openpyxl 
 
+class download_data:
 
-# Función que extrae los datos de determinado rango de filas y columnas de todos los exceles de una carpeta y da como output un dataframe
+    def __init__(self) :
 
-def extract_data(folder_path, row_min, row_max, year, sheet):
-    
-    dataset = []
+        self. meses = ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"]
+        self.años = ["19", "20", "21", "22"]
+        self._url_dane = "https://www.dane.gov.co/files/investigaciones/boletines/ipc/anexo_ipc_"
+        self.current_directory = os.getcwd().replace("\\","/")
+        self.excel_inflation_folder = os.path.join(os.getcwd(), "data", "excel_inflation")    
 
-    # recorremos todos los archivos dentro de la ruta indicada
-    for filename in os.listdir(folder_path):
-        # seleccionamos los archivos que terminen con el año seleccionado y que sean de tipo .xlxs
-        if filename.endswith(str(year)+'.xlsx'):
+    # Función que extrae los datos de determinado rango de filas y columnas de todos los exceles de una carpeta y da como output un dataframe
+    def extract_data(self,folder_path, row_min, row_max, year, sheet):
+        
+        dataset = []
 
-            # Creamos la ruta del archivo uniendo la ruta del directorio y el nombre del archivo    
-            file_path = os.path.join(folder_path, filename)
-            # Abrimos el excel con la ruta creada anteriormente
-            workbook = openpyxl.load_workbook(file_path)
-            # Generamos una lista con los nombres de todas las hojas del archivo .xlxs
-            sheet_names = workbook.sheetnames
+        # recorremos todos los archivos dentro de la ruta indicada
+        for filename in os.listdir(folder_path):
+            # seleccionamos los archivos que terminen con el año seleccionado y que sean de tipo .xlxs
+            if filename.endswith(str(year)+'.xlsx'):
 
-            # En caso de que el nombre de la hoja se encuentre en la lista generada anteriormente la seleccionamos
-            if sheet in sheet_names:
-                sheet_choice = workbook[sheet]
+                # Creamos la ruta del archivo uniendo la ruta del directorio y el nombre del archivo    
+                file_path = os.path.join(folder_path, filename)
+                # Abrimos el excel con la ruta creada anteriormente
+                workbook = openpyxl.load_workbook(file_path)
+                # Generamos una lista con los nombres de todas las hojas del archivo .xlxs
+                sheet_names = workbook.sheetnames
+
+                # En caso de que el nombre de la hoja se encuentre en la lista generada anteriormente la seleccionamos
+                if sheet in sheet_names:
+                    sheet_choice = workbook[sheet]
+                    
+                    # creamos una lista donde almacenaremos todos los datos extraidos de la hoja
+                    data = []
+
+                    # iteramos el rango de filas seleccionadas por el usuario y guardamos los datos de estas filas en la lista data
+                    for row in sheet_choice.iter_rows(min_row = row_min, max_row=row_max, values_only=True):
+                        data.append(row)
+
+                    # añadimos estos datos a dataset     
+                    dataset.extend(data)
                 
-                # creamos una lista donde almacenaremos todos los datos extraidos de la hoja
-                data = []
+                #Cerramos el excel
+                workbook.close()
+        
+        # Convertimos todo lo almacenado del año en un dataframe
+        df = pd.DataFrame(dataset)
+        return df
 
-                # iteramos el rango de filas seleccionadas por el usuario y guardamos los datos de estas filas en la lista data
-                for row in sheet_choice.iter_rows(min_row = row_min, max_row=row_max, values_only=True):
-                    data.append(row)
+    # Create excel_inflation folder
+    def creation_folder(self):
 
-                # añadimos estos datos a dataset     
-                dataset.extend(data)
-            
-            #Cerramos el excel
-            workbook.close()
+        #Extraemos directorio actual y en caso de no tener una carpeta donde alojar los .xlsx la creamos
+        self.excel_inflation_folder = self.current_directory + "/data/excel_inflation"
+        
+
+        # Create folder using os
+        try:
+            os.makedirs(self.excel_inflation_folder)
+            print(f"Carpeta '{self.excel_inflation_folder}' creada correctamente.")
+        except OSError as e:
+            print(f"Error al crear la carpeta: {e}")
+
+    # Check if there are data in the excel_infaltion folder for each month and year
+    def data_folder_check(self):
+        #Verificamos si existe algún archivo en la carpeta de destino, en caso negativo descargamos los .xlsx
+        
+        files_in_folder = os.listdir(self.excel_inflation_folder)
+        
+        if not files_in_folder:
+            for año in self.años:
+                for mes in self.meses:
+                    url = "https://www.dane.gov.co/files/investigaciones/boletines/ipc/anexo_ipc_"
+                    url = url + mes + año + ".xlsx"
+                    response = requests.get(url)
+
+                    with open(self.excel_inflation_folder + "/inflacion" + mes + año + ".xlsx" ,"wb") as file:
+                        file.write(response.content)
+        else:
+            print("La carpeta contiene archivos:")
+
+    # Download the data from the url
+    def download_sheets(self):        
+
+        # Check if the folder excel_inflation exists
     
-    # Convertimos todo lo almacenado del año en un dataframe
-    df = pd.DataFrame(dataset)
-    return df
+        if not os.path.exists(self.excel_inflation_folder) :
+            self.creation_folder()
+        
 
-#Declaramos las variables necesarias para generar la URL e iterar para descargar los .xlxs
-
-url = "https://www.dane.gov.co/files/investigaciones/boletines/ipc/anexo_ipc_"
-
-meses = ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"]
-años = ["19", "20", "21", "22"]
-
-#Extraemos directorio actual y en caso de no tener una carpeta donde alojar los .xlsx la creamos
-
-current_directory = os.getcwd()
-current_directory = current_directory.replace( "\\" , "/")
-excel_inflation_folder = current_directory + "/excel_inflation"
-
-if not os.path.isdir(excel_inflation_folder):
-    os.makedirs(excel_inflation_folder)
-    print("La carpeta fue creada exitosamente")
-else:
-    print("la carpeta ya existe")
-
-#Verificamos si existe algún archivo en la carpeta de destino, en caso negativo descargamos los .xlsx
-
-files_in_folder = os.listdir(excel_inflation_folder)
-
-if not files_in_folder:
-    for año in años:
-        for mes in meses:
-            url = "https://www.dane.gov.co/files/investigaciones/boletines/ipc/anexo_ipc_"
-            url = url + mes + año + ".xlsx"
-            response = requests.get(url)
-
-            with open(excel_inflation_folder + "/inflacion" + mes + año + ".xlsx" ,"wb") as file:
-                file.write(response.content)
-else:
-    print("La carpeta contiene archivos:")
+        self.data_folder_check()
 
 
-# Corremos la función los años necesarios
+    '''
 
-df_2022 = extract_data(excel_inflation_folder, 10,197,22,"8")
-df_2021 = extract_data(excel_inflation_folder, 10,197,21,"8")
-df_2020 = extract_data(excel_inflation_folder, 10,197,20,"8")
-df_2019 = extract_data(excel_inflation_folder, 10,197,19,"7")
+    # Corremos la función los años necesarios
 
-# Export DataFrame to CSV
-df_2019.to_csv(current_directory + "/df_2019.csv", index=False)
-df_2020.to_csv(current_directory + "/df_2020.csv", index=False)
-df_2021.to_csv(current_directory + "/df_2021.csv", index=False)
-df_2022.to_csv(current_directory + "/df_2022.csv", index=False)
+    df_2022 = extract_data(excel_inflation_folder, 10,197,22,"8")
+    df_2021 = extract_data(excel_inflation_folder, 10,197,21,"8")
+    df_2020 = extract_data(excel_inflation_folder, 10,197,20,"8")
+    df_2019 = extract_data(excel_inflation_folder, 10,197,19,"7")
+
+    # Export DataFrame to CSV
+    df_2019.to_csv(current_directory + "/df_2019.csv", index=False)
+    df_2020.to_csv(current_directory + "/df_2020.csv", index=False)
+    df_2021.to_csv(current_directory + "/df_2021.csv", index=False)
+    df_2022.to_csv(current_directory + "/df_2022.csv", index=False)
 
 
-# descargados todos los exceles debemos abrirlos, extraer los datos y pegarlos en un nuevo dataset añadiendo la columna data
- 
+    # descargados todos los exceles debemos abrirlos, extraer los datos y pegarlos en un nuevo dataset añadiendo la columna data
+    
 
-# c) osea que al correr este .py me descargue los archivos del DANE y despues los abra y me genere el xlsx que estoy necesitando, que ese sea el output
-# d) y luego que eliminé los archivos descargados, es decir una descarga temporal, porque no me interesa tenerlos en mi repositorio
-
+    # c) osea que al correr este .py me descargue los archivos del DANE y despues los abra y me genere el xlsx que estoy necesitando, que ese sea el output
+    # d) y luego que eliminé los archivos descargados, es decir una descarga temporal, porque no me interesa tenerlos en mi repositorio
+    '''
 
 
---
+++ backend/main.py
@@ -0,0 +1,7 @@
+from download_data import download_data
+
+
+if __name__ == '__main__':
+
+    descarga_datos = download_data()
+    descarga_datos.download_sheets()
